dad817d9d8987629e35b9cdea11722bd34edc7b8
RizalEka321 <hahaharizal6@gmail.com>
2023-10-24 09:42:41 +0700

    perbaikan

--- app/src/main/java/com/tripointeknologi/tsunami_tv/SignalActivity.java
+++ app/src/main/java/com/tripointeknologi/tsunami_tv/SignalActivity.java
@@ -40,6 +40,7 @@ import java.util.List;
 public class SignalActivity extends AppCompatActivity implements OnMapReadyCallback {
     private Dialog detail;
     private int currentLocationIndex = -1;
+    private boolean isCardVisible = false;
     private GoogleMap googleMap;
     private List<SignalData> signalData;
     private ArrayObjectAdapter rowsAdapter;
@@ -53,36 +54,36 @@ public class SignalActivity extends AppCompatActivity implements OnMapReadyCallb
         detail = new Dialog(this, R.style.CustomPopupTheme);
 
         signalData = new ArrayList<>();
-        signalData.add(new SignalData(new LatLng(-8.61306544945518, 114.06503372193593), "Signal 1", "Aktif", "Alamat Signal 1","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.59300681908755, 114.2389213385338), "Signal 2", "Perbaikan", "Alamat Signal 2","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.44626015184728, 114.34441315926983), "Signal 3", "Aktif", "Alamat Signal 3","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.747144280259468, 114.44063098689058), "Signal 4", "Aktif", "Alamat Signal 4","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.512682654119923, 113.820399878706), "Signal 5", "Aktif", "Alamat Signal 5","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.545956165457955, 113.91103708082358), "Signal 6", "Aktif", "Alamat Signal 6","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.478727109993299, 113.77096140482367), "Signal 7", "Tidak Aktif", "Alamat Signal 7","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.476010536705594, 114.1005512307058), "Signal 8", "Aktif", "Alamat Signal 8","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.481443664080569, 114.06484566623524), "Signal 9", "Perbaikan", "Alamat Signal 9","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.594842586240576, 114.36078987850192), "Signal 10", "Aktif", "Alamat Signal 10","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.52898024755826, 114.388942342796), "Signal 11", "Aktif", "Alamat Signal 11","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.636255315988258, 114.46172676267832), "Signal 12", "Tidak Aktif", "Alamat Signal 12","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.428723454520092, 114.0894495632817), "Signal 13", "Tidak Aktif", "Alamat Signal 13","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.263275841182562, 114.33974489526362), "Signal 14", "Aktif", "Alamat Signal 14","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.334943280597523, 114.21406468601312), "Signal 15", "Aktif", "Alamat Signal 15","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.433991331273027, 114.16081036005953), "Signal 16", "Perbaikan", "Alamat Signal 16","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.487719564540194, 114.01169824738948), "Signal 17", "Tidak Aktif", "Alamat Signal 17","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.25378946665479, 114.35252593349247), "Signal 18", "Tidak Aktif", "Alamat Signal 18","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.286463795911496, 114.27583970411929), "Signal 19", "Tidak Aktif", "Alamat Signal 19","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.409758504267158, 114.02660945865647), "Signal 20", "Perbaikan", "Alamat Signal 20","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.28540982759017, 114.24601728158528), "Signal 21", "Perbaikan", "Alamat Signal 21","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.501413796019042, 114.01595859346574), "Signal 22", "Aktif", "Alamat Signal 22","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.343373882890461, 114.19808838822703), "Signal 23", "Aktif", "Alamat Signal 23","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.250627291167588, 114.226845724242), "Signal 24", "Aktif", "Alamat Signal 24","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.404490297733256, 114.02021893954203), "Signal 25", "Aktif", "Alamat Signal 25","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.422896465676269, 113.99268555981146), "Signal 26", "Aktif", "Alamat Signal 26","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.453415222535932, 114.02230419246344), "Signal 27", "Aktif", "Alamat Signal 27","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.512614714018747, 114.24382688250634), "Signal 28", "Aktif", "Alamat Signal 28","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.46440138406977, 114.1969307141407), "Signal 29", "Aktif", "Alamat Signal 29","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
-        signalData.add(new SignalData(new LatLng(-8.521768458899842, 114.21976174347661), "Signal 30", "Aktif", "Alamat Signal 30","50v","26 derajat","04 Oktober 2023","Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.61306544945518, 114.06503372193593), "Signal 1", "Aktif", "Alamat Signal 1", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.59300681908755, 114.2389213385338), "Signal 2", "Perbaikan", "Alamat Signal 2", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.44626015184728, 114.34441315926983), "Signal 3", "Aktif", "Alamat Signal 3", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.747144280259468, 114.44063098689058), "Signal 4", "Aktif", "Alamat Signal 4", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.512682654119923, 113.820399878706), "Signal 5", "Aktif", "Alamat Signal 5", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.545956165457955, 113.91103708082358), "Signal 6", "Aktif", "Alamat Signal 6", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.478727109993299, 113.77096140482367), "Signal 7", "Tidak Aktif", "Alamat Signal 7", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.476010536705594, 114.1005512307058), "Signal 8", "Aktif", "Alamat Signal 8", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.481443664080569, 114.06484566623524), "Signal 9", "Perbaikan", "Alamat Signal 9", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.594842586240576, 114.36078987850192), "Signal 10", "Aktif", "Alamat Signal 10", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.52898024755826, 114.388942342796), "Signal 11", "Aktif", "Alamat Signal 11", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.636255315988258, 114.46172676267832), "Signal 12", "Tidak Aktif", "Alamat Signal 12", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.428723454520092, 114.0894495632817), "Signal 13", "Tidak Aktif", "Alamat Signal 13", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.263275841182562, 114.33974489526362), "Signal 14", "Aktif", "Alamat Signal 14", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.334943280597523, 114.21406468601312), "Signal 15", "Aktif", "Alamat Signal 15", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.433991331273027, 114.16081036005953), "Signal 16", "Perbaikan", "Alamat Signal 16", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.487719564540194, 114.01169824738948), "Signal 17", "Tidak Aktif", "Alamat Signal 17", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.25378946665479, 114.35252593349247), "Signal 18", "Tidak Aktif", "Alamat Signal 18", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.286463795911496, 114.27583970411929), "Signal 19", "Tidak Aktif", "Alamat Signal 19", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.409758504267158, 114.02660945865647), "Signal 20", "Perbaikan", "Alamat Signal 20", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.28540982759017, 114.24601728158528), "Signal 21", "Perbaikan", "Alamat Signal 21", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.501413796019042, 114.01595859346574), "Signal 22", "Aktif", "Alamat Signal 22", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.343373882890461, 114.19808838822703), "Signal 23", "Aktif", "Alamat Signal 23", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.250627291167588, 114.226845724242), "Signal 24", "Aktif", "Alamat Signal 24", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.404490297733256, 114.02021893954203), "Signal 25", "Aktif", "Alamat Signal 25", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.422896465676269, 113.99268555981146), "Signal 26", "Aktif", "Alamat Signal 26", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.453415222535932, 114.02230419246344), "Signal 27", "Aktif", "Alamat Signal 27", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.512614714018747, 114.24382688250634), "Signal 28", "Aktif", "Alamat Signal 28", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.46440138406977, 114.1969307141407), "Signal 29", "Aktif", "Alamat Signal 29", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
+        signalData.add(new SignalData(new LatLng(-8.521768458899842, 114.21976174347661), "Signal 30", "Aktif", "Alamat Signal 30", "50v", "26 derajat", "04 Oktober 2023", "Sudah Bisa Digunakan", new Date()));
 
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_maps);
@@ -156,10 +157,8 @@ public class SignalActivity extends AppCompatActivity implements OnMapReadyCallb
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_DPAD_UP) {
-            showcard();
-        } else if (keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
-            hidecard();
+        if (keyCode == KeyEvent.KEYCODE_DPAD_UP || keyCode == KeyEvent.KEYCODE_DPAD_DOWN) {
+            toggleCardVisibility();
         } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
             if (currentLocationIndex > 0) {
                 currentLocationIndex--;
@@ -172,24 +171,39 @@ public class SignalActivity extends AppCompatActivity implements OnMapReadyCallb
                 LatLng targetLatLng = signalData.get(currentLocationIndex).getLatLng();
                 moveCamera(targetLatLng);
             }
-        } else if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
-            if (currentLocationIndex >= 0 && currentLocationIndex < signalData.size()) {
-                SignalData signal = signalData.get(currentLocationIndex);
-                detail(signal);
-                return true;
-            }
         }
         return super.onKeyDown(keyCode, event);
     }
 
+        @Override
+        public boolean onKeyUp(int keyCode, KeyEvent event) {
+            if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+                if (currentLocationIndex >= 0 && currentLocationIndex < signalData.size()) {
+                    SignalData signal = signalData.get(currentLocationIndex);
+                    detail(signal);
+                }
+            }
+            return super.onKeyUp(keyCode, event);
+        }
 
-    private void showcard() {
+
+
+        private void showcard() {
         loadRows();
         rowsFragment.getView().setVisibility(View.VISIBLE);
     }
 
     private void hidecard() {
         rowsFragment.getView().setVisibility(View.GONE);
+    }
+
+    private void toggleCardVisibility() {
+        isCardVisible = !isCardVisible;
+        if (isCardVisible) {
+            showcard();
+        } else {
+            hidecard();
+        }
     }
 
     private void detail(SignalData signalData) {
@@ -208,12 +222,12 @@ public class SignalActivity extends AppCompatActivity implements OnMapReadyCallb
 
         // Customize the title and content
         title.setText("Signal Details");
-        nama.setText(String.format(": %s",signalData.getName()));
-        status.setText(String.format(": %s",signalData.getStatus()));
-        voltase.setText(String.format(": %s",signalData.getVoltase()));
-        temperatur.setText(String.format(": %s",signalData.getTemperatur()));
-        tanggal_akktifasi.setText(String.format(": %s",signalData.getTanggal_aktifasi()));
-        keterangan.setText(String.format(": %s",signalData.getKeterangan()));
+        nama.setText(String.format(": %s", signalData.getName()));
+        status.setText(String.format(": %s", signalData.getStatus()));
+        voltase.setText(String.format(": %s", signalData.getVoltase()));
+        temperatur.setText(String.format(": %s", signalData.getTemperatur()));
+        tanggal_akktifasi.setText(String.format(": %s", signalData.getTanggal_aktifasi()));
+        keterangan.setText(String.format(": %s", signalData.getKeterangan()));
 
         // Customize the dialog appearance
         Window window = detail.getWindow();
